66ccff2722cf5bab04b84da25ad59969e383dc1b
akash <akash@gmail.com>
2023-03-28 11:48:18 +0530

    api addded

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
-
-app.get('/api', (req, res) => res.send('Hello World!'))
+app.use(express.static("public"))
+app.get('/api/user', (req, res) => res.send('Hello World!'))
+app.get('/api/blogs', (req, res) => res.json({
+    message: "all blogs fetched",
+    data: [
+        { id: 1 },
+        { id: 2 },
+        { id: 3 },
+    ]
+}))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
